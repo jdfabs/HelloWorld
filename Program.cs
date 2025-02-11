effcd5c46bba585eb6c9dbdf3f09fa44a9a70d2e
jdfabs <jdfabs1@gmail.com>
2025-02-11 16:54:33 +0000

    Second WriteLine

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, GitHub!");
+            Console.WriteLine("New WriteLine for commit");
         }
     }
 }
